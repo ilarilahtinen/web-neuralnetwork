599a1599454265417b617f50364872217a1be8d4
ilari <ilari.k.lahtinen@gmail.com>
2018-10-12 14:40:40 +0300

    Added zoom feature

--- nn.js
+++ nn.js
@@ -5,6 +5,7 @@ class neuralNet{
     this.output=output;
     this.dx=0;
     this.dy=0;
+    this.scale=1;
     this.inTohid=new Matrix(input,hidden)
     this.hidToout=new Matrix(hidden,output)
     this.inTohid.randomize()
@@ -13,6 +14,10 @@ class neuralNet{
   move(x,y,ctx){
     this.dx+=x;
     this.dy+=y;
+    this.draw(ctx)
+  }
+  resize(n,ctx){
+    this.scale+=n
     this.draw(ctx)
   }
   predict(input){
@@ -25,11 +30,11 @@ class neuralNet{
   draw(ctx){
     ctx.clearRect(0,0,300,300)
     ctx.fillStyle="black"
-    let inStart=150-(this.input*30/2)+this.dy;
-    let inhid=150-(this.hidden*30/2)+this.dy;
-    let inout=150-(this.output*30/2)+this.dy;
+    let inStart=150-(this.input*30/2)*this.scale+this.dy;
+    let inhid=150-(this.hidden*30/2)*this.scale+this.dy;
+    let inout=150-(this.output*30/2)*this.scale+this.dy;
     for(let i=0; i<this.input; i++){
-      ctx.fillRect(10+this.dx,inStart+30*i,20,20)
+      ctx.fillRect(10*this.scale+this.dx,inStart+30*this.scale*i,20*this.scale,20*this.scale)
       for(let j=0; j<this.hidden; j++){
         if(this.inTohid.data[i][j]>0){
           ctx.strokeStyle="green"
@@ -38,15 +43,15 @@ class neuralNet{
           ctx.strokeStyle="red"
         }
         ctx.beginPath();
-        ctx.moveTo(30+this.dx,inStart+30*i+10);
-        ctx.lineTo(60+this.dx,inhid+30*j+10);
+        ctx.moveTo(30*this.scale+this.dx,inStart+30*i*this.scale+10);
+        ctx.lineTo(60*this.scale+this.dx,inhid+30*j*this.scale+10);
         ctx.stroke();
 
       }
     }
 
     for(let i=0; i<this.hidden; i++){
-      ctx.fillRect(60+this.dx,inhid+30*i,20,20)
+      ctx.fillRect(60*this.scale+this.dx,inhid+30*this.scale*i,20*this.scale,20*this.scale)
       for(let j=0; j<this.output; j++){
         if(this.hidToout.data[i][j]>0){
           ctx.strokeStyle="green"
@@ -55,10 +60,10 @@ class neuralNet{
           ctx.strokeStyle="red"
         }
         ctx.beginPath();
-        ctx.moveTo(80+this.dx,inhid+30*i+10);
-        ctx.lineTo(110+this.dx,inout+30*j+10);
+        ctx.moveTo(80*this.scale+this.dx,inhid+30*i*this.scale+10);
+        ctx.lineTo(110*this.scale+this.dx,inout+30*j*this.scale+10);
         ctx.stroke();
-        ctx.fillRect(110+this.dx,inout+30*j,20,20)
+        ctx.fillRect(110*this.scale+this.dx,inout+30*j*this.scale,20*this.scale,20*this.scale)
       }
     }
 
--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let nn=new neuralNet(2,10,1);
 nn.draw(ctx)
 nn.predict([1,2])
 window.onkeyup = (e)=>{
+  
   switch(e.key){
     case "ArrowDown":
       nn.move(0,5,ctx);
@@ -17,6 +18,11 @@ window.onkeyup = (e)=>{
     case "ArrowRight":
       nn.move(5,0,ctx);
       break
-
+    case "w":
+      nn.resize(-0.1,ctx);
+      break
+    case "e":
+      nn.resize(0.1,ctx);
+      break
   }
 }
