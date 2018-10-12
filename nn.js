722eb0fb182b85d39540c315f62f21a8a56b2427
ilari <ilari.k.lahtinen@gmail.com>
2018-10-12 14:12:24 +0300

    Adding possibility to move network with arrow keys.

--- nn.js
+++ nn.js
@@ -3,12 +3,18 @@ class neuralNet{
     this.input=input;
     this.hidden=hidden;
     this.output=output;
+    this.dx=0;
+    this.dy=0;
     this.inTohid=new Matrix(input,hidden)
     this.hidToout=new Matrix(hidden,output)
     this.inTohid.randomize()
     this.hidToout.randomize()
   }
-
+  move(x,y,ctx){
+    this.dx+=x;
+    this.dy+=y;
+    this.draw(ctx)
+  }
   predict(input){
     let inp=Matrix.arrayToMatrix(input);
     let mid=Matrix.mult(inp, this.inTohid);
@@ -17,12 +23,13 @@ class neuralNet{
   }
 
   draw(ctx){
+    ctx.clearRect(0,0,300,300)
     ctx.fillStyle="black"
-    let inStart=150-(this.input*30/2)
-    let inhid=150-(this.hidden*30/2)
-    let inout=150-(this.output*30/2)
+    let inStart=150-(this.input*30/2)+this.dy;
+    let inhid=150-(this.hidden*30/2)+this.dy;
+    let inout=150-(this.output*30/2)+this.dy;
     for(let i=0; i<this.input; i++){
-      ctx.fillRect(10,inStart+30*i,20,20)
+      ctx.fillRect(10+this.dx,inStart+30*i,20,20)
       for(let j=0; j<this.hidden; j++){
         if(this.inTohid.data[i][j]>0){
           ctx.strokeStyle="green"
@@ -31,15 +38,15 @@ class neuralNet{
           ctx.strokeStyle="red"
         }
         ctx.beginPath();
-        ctx.moveTo(30,inStart+30*i+10);
-        ctx.lineTo(60,inhid+30*j+10);
+        ctx.moveTo(30+this.dx,inStart+30*i+10);
+        ctx.lineTo(60+this.dx,inhid+30*j+10);
         ctx.stroke();
 
       }
     }
 
     for(let i=0; i<this.hidden; i++){
-      ctx.fillRect(60,inhid+30*i,20,20)
+      ctx.fillRect(60+this.dx,inhid+30*i,20,20)
       for(let j=0; j<this.output; j++){
         if(this.hidToout.data[i][j]>0){
           ctx.strokeStyle="green"
@@ -48,10 +55,10 @@ class neuralNet{
           ctx.strokeStyle="red"
         }
         ctx.beginPath();
-        ctx.moveTo(80,inhid+30*i+10);
-        ctx.lineTo(110,inout+30*j+10);
+        ctx.moveTo(80+this.dx,inhid+30*i+10);
+        ctx.lineTo(110+this.dx,inout+30*j+10);
         ctx.stroke();
-        ctx.fillRect(110,inout+30*j,20,20)
+        ctx.fillRect(110+this.dx,inout+30*j,20,20)
       }
     }
 
--- script.js
+++ script.js
@@ -3,3 +3,20 @@ let ctx=canvas.getContext("2d");
 let nn=new neuralNet(2,10,1);
 nn.draw(ctx)
 nn.predict([1,2])
+window.onkeyup = (e)=>{
+  switch(e.key){
+    case "ArrowDown":
+      nn.move(0,5,ctx);
+      break
+    case "ArrowUp":
+      nn.move(0,-5,ctx);
+      break
+    case "ArrowLeft":
+      nn.move(-5,0,ctx);
+      break
+    case "ArrowRight":
+      nn.move(5,0,ctx);
+      break
+
+  }
+}
